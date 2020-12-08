b9c01eb864f20e59f3a88401191dd2fb58167081
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-12-08 18:42:14 +0000

    wrote and passed 'should calculate paint stock'by writing totalPaintStock method in Painter

--- models/painter.js
+++ models/painter.js
@@ -7,4 +7,14 @@ Painter.prototype.addPaint = function(newPaint){
     this.paintStock.push(newPaint);
 };
 
+Painter.prototype.totalPaintStock = function(){
+    let totalPaint = 0
+    for (let paint of this.paintStock) {
+        let paintAmount = paint.numLitres;
+        totalPaint += paintAmount;
+    } 
+    return totalPaint;
+};
+
+
 module.exports = Painter;
--- models/specs/painter_spec.js
+++ models/specs/painter_spec.js
@@ -3,25 +3,39 @@ const Painter = require('../painter');
 const Paint = require('../paint');
 
 describe("Painter", function(){
+    //arrange
+    let leonThePainter;
+    let redPaint;
+    let bluePaint;
+
+    beforeEach(function(){
+        leonThePainter = new Painter("Leon");
+        redPaint = new Paint(5);
+        bluePaint = new Paint(10);
+    });
+
     it("should have empty stock", function(){
-        //arrange
-        daveThePainter = new Painter("Dave");
         //act
-        stock = daveThePainter.paintStock;
+        stock = leonThePainter.paintStock;
         //assert
         assert.deepStrictEqual(stock, [])
     });
 
     it("should be able to add stock", function(){
-        //arrange
-        bluePaint = new Paint(10);
-        leonThePainter = new Painter("Leon");
         //act
         leonThePainter.addPaint(bluePaint);
         stock = leonThePainter.paintStock;
         //assert
         assert.deepStrictEqual(stock, [bluePaint])
-        
+    });
+    
+    it("should calculate paint stock", function(){
+        //act
+        leonThePainter.addPaint(redPaint);
+        leonThePainter.addPaint(bluePaint);
+        totalLitres = leonThePainter.totalPaintStock();
+        //assert
+        assert.strictEqual(totalLitres, 15)
 
     });
 
